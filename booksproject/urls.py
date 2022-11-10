5e4f6e1e0c1ca622dceec5de37a68bed6f95866f
Paul <x00186923@mytudublin.ie>
2022-11-10 09:49:42 +0000

    lab 9 part 4 commit

--- booksproject/urls.py
+++ booksproject/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,5 +25,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
